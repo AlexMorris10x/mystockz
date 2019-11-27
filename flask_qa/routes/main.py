733be7dcba617b1e5666f9a9c1087e0d296c86eb
Alex Morris <alexmorris10x@Alexs-MBP.noise>
2019-11-26 16:31:37 -0800

    .

--- flask_qa/routes/main.py
+++ flask_qa/routes/main.py
@@ -40,29 +40,30 @@ def index():
         db.session.query(
             Stocks.share,
             Stocks.shares,
-            Stocks.price
+            Stocks.price,
+            db.func.sum(Stocks.shares).label("shares")
         )
-        # .group_by(Stocks.share)
+        .group_by(Stocks.share)
         .filter(Stocks.name == current_user.name)
         .order_by(Stocks.share)
         .all()
         # .values(db.func.sum(Stocks.shares).label("shares"))
     )
 
-    currentCash = db.session.query(User).filter(
-        User.name == current_user.name).all()
+    # currentCash = db.session.query(User).filter(
+    #     User.name == current_user.name).all()
 
-    totalStocks = (
-        db.session.query(db.func.sum(
-            Stocks.shares * Stocks.price).label("totalStocks"))
-        .filter(Stocks.name == current_user.name).all()
-    )
+    # totalStocks = (
+    #     db.session.query(Stocks.shares, Stocks.price, db.func.sum(
+    #         Stocks.shares * Stocks.price).label("totalStocks"))
+    #     .filter(Stocks.name == current_user.name).all()
+    # )
 
     return render_template(
         "home.html",
         userStocks=userStocks,
-        currentCash=currentCash,
-        totalStocks=totalStocks,
+        # currentCash=currentCash,
+        # totalStocks=totalStocks,
     )
 
 
